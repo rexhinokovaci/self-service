c2ee65a72e830859b9522a58d1e91bdb69ae4ca8
Rexhino Kovaci <kovacirexhino@gmail.com>
2023-03-01 15:22:20 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function divClicked() {
+    alert("Kafja jote u porosit");
+    console.log("Hej Syri Syri")
+}
+
+function divClicked1() {
+    alert("kafja juaj eshte 5 min larg jush me afer se miranda");
+    console.log("Tkam pa te myslym shyri")
+}
